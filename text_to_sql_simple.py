c46bf16db5456a60dd6c43606cf46579ab40e9a0
LC2792 <leonardo_jcantillo@soy.sena.edu.co>
2025-11-20 17:43:15 -0500

    Corrección: Arreglado un error de sintaxis menor

--- text_to_sql_simple.py
+++ text_to_sql_simple.py
@@ -6,14 +6,12 @@ Base = declarative_base()
 engine = create_engine("sqlite:///personas.db", echo=True)
 Session = sessionmaker(bind=engine)
 session = Session()
-# Cambio hecho por Juan David Moreno Carpio para prueba de colaboración
-#comentario de porueba 2
-#cambio 3 juan
+#cambios por decima vez#
 
 # --- Definición del modelo ---
 class Persona(Base):
     __tablename__ = "personas"
-# Cambio realizado por Juan para prueba
+
 
     id = Column(Integer, primary_key=True)
     nombre = Column(String)
